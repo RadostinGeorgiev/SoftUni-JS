8a46d031cd361fd35496b4d49da26d62b027656f
Radostin Georgiev <radostin.r.georgiev@gmail.com>
2021-05-31 23:58:11 +0300

    Add More Exercise

---
+++ Data Types and Variables/More Exercise/02. Prime Number Checker.js
@@ -0,0 +1,17 @@
+function primeNumberChecker(number) {
+
+        let isPrime = true;
+        for (let index = 2; index*index <= number; index++)
+        {
+            if (number % index == 0)
+            {
+                isPrime = false;
+                break;
+            }
+        }
+        console.log(isPrime);
+}
+
+primeNumberChecker(7);
+primeNumberChecker(8);
+primeNumberChecker(81);
---
+++ Data Types and Variables/More Exercise/07. Triangle Area.js
@@ -0,0 +1,12 @@
+function triangleArea(sideA, sideB, sideC) {
+
+    let semiPerimeter = (sideA + sideB + sideC) / 2;
+    let area = Math.sqrt(semiPerimeter
+        * (semiPerimeter - sideA)
+        * (semiPerimeter - sideB)
+        * (semiPerimeter - sideC));
+
+    console.log(area);
+}
+
+triangleArea(2, 3.5, 4);
