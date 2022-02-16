d34012f1303e6f341533138e935326788ca2fd85
Radostin Georgiev <radostin.r.georgiev@gmail.com>
2022-02-16 23:31:02 +0200

    Initial commit

---
+++ JS Advanced/12. Advanced Functions - Exercise/02. Argument Info.js
@@ -0,0 +1,19 @@
+function argumentInfo(...input) {
+    let types = {};
+
+    input.forEach(v => {
+        const type = typeof (v);
+        console.log(`${type}: ${v}`);
+
+        if (!types.hasOwnProperty(type)) {
+            types[type] = 0;
+        }
+
+        types[type]++;
+    });
+
+    Object.keys(types).sort((a, b) => types[b] - types[a])
+        .forEach(k => console.log(`${k} = ${types[k]}`));
+}
+
+argumentInfo(56, 'cat', 42, function () { console.log('Hello world!'); });
