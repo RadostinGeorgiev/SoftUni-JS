9a42e6b7b52b1ac38f249fde7e5b5f40f9ea0a91
Radostin Georgiev <radostin.r.georgiev@gmail.com>
2022-05-19 00:38:35 +0300

    Small code optimization

--- JS Advanced/02. Results for Syntax, Functions and Statements - Exercise/07. Cooking by Numbers.js
+++ JS Advanced/02. Results for Syntax, Functions and Statements - Exercise/07. Cooking by Numbers.js
@@ -1,17 +1,18 @@
-function cookingByNumbers(number, ...operations) {
+function cookingByNumbers(number, ...input) {
+    const operations = {
+        'chop': (value) => value / 2,
+        'dice': (value) => Math.sqrt(value),
+        'spice': (value) => value + 1,
+        'bake': (value) => value * 3,
+        'fillet': (value) => value * 0.8,
+    }
+    
     number = Number(number);
 
-    while (operation = operations.shift()) {
-        switch (operation) {
-            case 'chop': number /= 2; break;
-            case 'dice': number = Math.sqrt(number); break;
-            case 'spice': number += 1; break;
-            case 'bake': number *= 3; break;
-            case 'fillet': number -= number * 0.2; break;
-        }
-
-        console.log(number);
-    }
+    input.forEach(x => {
+        number = operations[x](number);
+        console.log(number); 
+    });
 }
 
 cookingByNumbers('32', 'chop', 'chop', 'chop', 'chop', 'chop');
